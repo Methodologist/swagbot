531b96d63a948fffe10ffa6fac549618f1474050
Christopher Gillig <ohio2k@hotmail.com>
2016-08-23 13:12:09 -0400

    Update swagbot.py

--- swagbot.py
+++ swagbot.py
@@ -3,17 +3,18 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import *
+from selenium.webdriver.chrome.options import Options
 
 
 class PythonOrgSearch(unittest.TestCase):
     #Implemented ChromeDriver, firefoxdriver is outdated and now called marionettedriver, also known as GeckoDriver
     def setUp(self):
-        self.driver = webdriver.Chrome(executable_path="C:\drivers\chromedriver.exe")
+        self.driver = webdriver.Chrome(executable_path="C:\\drivers\\chromedriver.exe")
 
     def test_search(self):
         driver = self.driver
-        driver.implicitly_wait(10)
-        accounts = {'youremail': 'yourpass'}
+        driver.implicitly_wait(10000)
+        accounts = {'': ''}
         for emaila, passw in accounts.items():
             driver.get("http://www.swagbucks.com/p/login")
             driver.maximize_window()
@@ -27,12 +28,13 @@ class PythonOrgSearch(unittest.TestCase):
             getpassword.send_keys(Keys.RETURN)
             driver.get("http://www.swagbucks.com/watch/playlists/111/editors-pick")
             while "Swagbucks" in driver.title:
-                driver.implicitly_wait(10000)
                 driver.switch_to.window(driver.window_handles[0])
                 i = 0
                 listhead = driver.find_elements_by_xpath(".//*[contains(@id,'sbHomeCard')]")
                 for head in listhead:
                     if head != listhead[len(listhead)-1]:
+                        driver.switch_to.window(driver.window_handles[0])
+                        print(driver.title)
                         i += 1
                         print(str(head.text) + '\n' + "_" * 15)
                         limit = driver.find_element_by_xpath("/html/body/div[3]/div[2]/div[1]/div[1]/main/div[2]/div[1]/section[{}]/span/var".format(i))
@@ -42,15 +44,17 @@ class PythonOrgSearch(unittest.TestCase):
                         driver.find_element_by_xpath("/html/body/aside/article[{}]/ul/li[1]/a".format(i)).send_keys(Keys.CONTROL + Keys.ENTER)
                         ActionChains(driver).key_down(Keys.CONTROL).send_keys(Keys.TAB).key_up(Keys.CONTROL).perform()
                         driver.switch_to.window(driver.window_handles[1])
+                        time.sleep(1)
                         totalchecks = driver.find_elements_by_class_name('iconCheckmark')
-                        if vidcount != len(totalchecks):
-                            driver.find_element_by_id('wayToGo') or driver.find_element_by_id('vdb_501707fc-4361-4b38-912e-0df55098fb15') or driver.find_element_by_class_name('ulive-error-container')
+                        if len(totalchecks) != vidcount:
+                            ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+                            driver.find_element_by_id('wayToGo')
                         driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 'w')
-                        time.sleep(3)
+                        time.sleep(1)
                         driver.switch_to.window(driver.window_handles[0])
                         logo = driver.find_element_by_id("sbLogo")
                         ActionChains(driver).move_to_element(logo).perform()
-                        time.sleep(3)
+                        time.sleep(1)
                     else:
                         driver.switch_to.window(driver.window_handles[0])
                         listhead.clear()
